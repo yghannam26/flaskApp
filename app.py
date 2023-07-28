3245a69f8b06e9cc9c7061f9fd11bda18fc630f8
yghannam26 <115160537+yghannam26@users.noreply.github.com>
2023-07-28 12:54:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/') 
 
 def hello(): 
-    return ('\nHello from Container to the World y!\n\n')
+    return ('\nHello from Container to the World you!\n\n')
 
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=8080, debug=True)
